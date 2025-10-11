887389ba08ab2ac964c0f3c24142324c0432d196
Nadeeshana-Lahiru <nadeeshana1998@gmail.com>
2025-10-11 16:07:30 +0530

    Complete the Header section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import { Route, Router, Routes } from 'react-router-dom'
+import { Route , Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
@@ -14,7 +14,7 @@ const App = () => {
         {/* give the path and element where need to go */}
         < Route path='/' element={<Home/>} />
         < Route path='/cart' element={<Cart/>} />
-        < Router path='/order' element={<PlaceOrder/>} />
+        < Route path='/order' element={<PlaceOrder/>} />
       </Routes>
     </div>
   )
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,11 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-        
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Chose from a diverse menu featuring a delectable array of dished crafted with the fitnest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate dining experience, one delicious mean at a time.</p>
+            <button>View Menu</button> 
+        </div>
     </div>
   )
 }
