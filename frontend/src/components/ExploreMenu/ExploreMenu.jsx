ff6ed6ccc4a23f989bf7f32c62a6e3206cbcecd3
Nadeeshana-Lahiru <nadeeshana1998@gmail.com>
2025-10-14 23:18:06 +0530

    Complete the Exploremenu component with category use state

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
 
-const ExploreMenu = () => {
+// pass the category and setCategory function
+const ExploreMenu = ({category,setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
         <h1>Explore our menu</h1>
@@ -12,9 +13,11 @@ const ExploreMenu = () => {
             {/* use menu_list to map using item object and index */}
             {menu_list.map((item,index)=>{
                 return (
-                    <div key={index} className="explore-menu-list-item">
+                    //pass the setCategory function. check previous state is same as the item menu name, if not same, set the state menu name
+                    <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
                         {/* item is object , menu_image is property */}
-                        <img src={item.menu_image} alt="" />
+                        {/* check the categoty is equal to item menu name, if equal state is active */}
+                        <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
                         <p>{item.menu_name}</p>
                     </div>
                 )
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 const Home = () => {
+
+  //create state variable name is category, setter function name is setcategory
+  const [category,setCategory] = useState("All");
+
   return (
     <div>
         {/* mount the Header component */}
         <Header />
-        <ExploreMenu />
+        {/* pass the category state and setter function to ExploreMenu component */}
+        <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
