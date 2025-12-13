33375cad4502b678768a3bd71ec65e45f0b60a08
Nadeeshana-Lahiru <nadeeshana1998@gmail.com>
2025-12-14 00:53:30 +0530

    Create addToCart and removeFromCart functionality for cartItems with useState

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 //create one context name is StoreContext
@@ -6,6 +6,25 @@ export const StoreContext = createContext(null);
 
 //create StoreContextProvider function
 const StoreContextProvider = (props) => {
+
+    // useState for ItemCount from the FoodItem.jsx , name is cartItem / setter function name is setCartItem
+    const [cartItems,setCartItems] = useState({});
+
+    // functionality for add to cart
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev)=>({...prev,[itemId]:1}))  // add item first time this happen
+        }
+        else{
+            // if item allready have and cuantity is 1 we will increa item key
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        }
+    }
+
+    // functionality for remove from cart
+    const removeFromCart = (itemId) => {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
 
     // create variable name contextValue
     const contextValue = {
