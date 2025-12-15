3455290e72785029bd1d50abc022a9833b5e5536
Nadeeshana-Lahiru <nadeeshana1998@gmail.com>
2025-12-15 23:38:50 +0530

    Create Routes for Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
 
@@ -14,10 +15,10 @@ const Navbar = () => {
             {/* compare the menu, if the state is home, active class will apply only that li tag, not other tags */}
             {/* onClick for logic to when click the particular menu that will active */}
             {/* inside onClick we call setMenu function to check the state */}
-            <li onClick={()=>setMenu("home")} className={menu==="home"?"active":" "}>home</li>
-            <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":" "}>menu</li>
-            <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":" "}>mobile-app</li>
-            <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":" "}>contact us</li>
+            <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":" "}>home</Link>
+            <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":" "}>menu</a>
+            <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":" "}>mobile-app</a>
+            <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":" "}>contact us</a>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
