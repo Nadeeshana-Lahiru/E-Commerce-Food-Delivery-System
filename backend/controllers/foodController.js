7b15a1e1bb6f084c03bd800190fc64116ef370d7
Nadeeshana-Lahiru <nadeeshana1998@gmail.com>
2025-12-20 02:01:58 +0530

    create logic for add Food function inside foodController.js fro store product data in database

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -7,6 +7,22 @@ import fs from 'fs'   // import file system
 
 const addFood = async (req,res) => {
 
+    let image_filename = `${req.file.filename}`;   // store uploaded file name
+    
+    const food = new foodModel({
+        name:req.body.name,
+        description:req.body.description,
+        price:req.body.price,
+        category:req.body.category,
+        image:image_filename
+    })
+    try {
+        await food.save();
+        res.json({success:true,message:"Food Added"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
 }
 
 export {addFood}
