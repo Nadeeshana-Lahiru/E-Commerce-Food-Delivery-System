c480c0f25658271a0fc4a7639ead7d29a55cf26b
Nadeeshana-Lahiru <nadeeshana1998@gmail.com>
2025-12-21 23:28:37 +0530

    Create admin panel Sidebar and Navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/sidebar/sidebar'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar/>
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+      </div>
     </div>
   )
 }
