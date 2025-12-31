3a3589e8c679c30ba278e7ccefb779d80c5fefde
Nadeeshana-Lahiru <nadeeshana1998@gmail.com>
2025-12-31 22:52:04 +0530

    Impliment the Navbar with profile if user Login

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -9,7 +9,8 @@ const Navbar = ({setShowLogin}) => {
     //create state variable - menu , setter function - setMenu, use useState , initialize variable "home"
     const [menu,setMenu] = useState("home");
 
-    const {getTotalCartAmount} = useContext(StoreContext);
+    // pass the token 
+    const {getTotalCartAmount,token,setToken} = useContext(StoreContext);
 
   return (
     <div className='navbar'>
@@ -29,7 +30,17 @@ const Navbar = ({setShowLogin}) => {
                 <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
                 <div className={getTotalCartAmount()===0?"":"dot"}></div>
             </div>
-            <button onClick={()=>setShowLogin(true)}>Sign in</button>
+            {/* if token not available show sign in button */}
+            {!token?<button onClick={()=>setShowLogin(true)}>Sign in</button>
+            :<div className='navbar-profile'>
+                <img src={assets.profile_icon} alt="" />
+                <ul className="nav-profile-dropdown">
+                    <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+                    <hr />
+                    <li><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+                </ul>
+                </div>}
+            
         </div>
     </div>
   )
